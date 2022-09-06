bc51eb79eb7f492f80083b13cb6d5106caa45681
Joshua <ezejoshuac@gmail.com>
2022-09-06 12:52:34 +0100

    Remove scroll from body for active hamburger

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const navMenu = document.querySelector('.nav-link-container');
 const toolBar = document.querySelector('.top-level');
 const navLinks = document.querySelectorAll('.nav-link');
 const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   toolBar.classList.toggle('active');
   logo.classList.toggle('active');
+  body.classList.toggle('lock-scroll');
 });
 
 navLinks.forEach((navLink) => {
@@ -17,5 +19,6 @@ navLinks.forEach((navLink) => {
     navMenu.classList.toggle('active');
     toolBar.classList.toggle('active');
     logo.classList.toggle('active');
+    body.classList.toggle('lock-scroll');
   });
 });

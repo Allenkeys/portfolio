bcb5076019d2adea9050c5cfe8af51b04f1873ab
Zuhaib Amjad <umar0423@gmail.com>
2022-09-06 15:35:38 +0500

    Add the mobile-menu links functionality

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-link-container");
-const toolBar = document.querySelector(".top-level");
-const logo = document.querySelector(".logo");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-link-container');
+const toolBar = document.querySelector('.top-level');
+const navLinks = document.querySelectorAll('.nav-link');
+const logo = document.querySelector('.logo');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    toolBar.classList.toggle("active");
-    logo.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  toolBar.classList.toggle('active');
+  logo.classList.toggle('active');
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    toolBar.classList.toggle('active');
+    logo.classList.toggle('active');
+  });
+});

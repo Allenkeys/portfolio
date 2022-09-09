2ee4f199c429a51e1822632402d090964c00f62b
Zuhaib Amjad <umar0423@gmail.com>
2022-09-09 20:28:36 +0500

    Add some bugs in form and Add functionality also

--- js/main.js
+++ js/main.js
@@ -50,6 +50,7 @@ function validateInput() {
   if (/^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g.test(emailValue)) {
     setSuccess(email);
     form.submit();
+    form.reset();
   } else {
     setError(email, 'Invalid email');
   }
